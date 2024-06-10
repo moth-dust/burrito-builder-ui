d75c826574d3e4717d01867aeb7fbb1a5334e1cf
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-06-10 09:56:47 -0600

    feat: basic intercept for get request.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe("empty spec", () => {
+describe("Page Load", () => {
+  beforeEach(()=>{
+    cy.intercept("GET", "http://localhost:3001/api/v1/orders",{
+      statusCode: 200, 
+      fixture: 'orders.json'
+    }).as('getData')
+  })
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
+    cy.visit("http://localhost:3000/");
+    cy.wait('@getData')
   });
 });
